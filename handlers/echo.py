24ec6824182dbeec12a574e75c863abefba1043d
coo1122 <adiletusenov50@gmail.com>
2025-02-09 02:08:05 -0800

    HW 2

--- handlers/echo.py
+++ handlers/echo.py
@@ -1,15 +1,15 @@
 from aiogram import types, Dispatcher
 
+games = ['⚽', '🎰', '🏀', '🎯', '🎳', '🎲']
 
-
-# @dp.message_handler()
-async def echo_handler(message:types.Message):
+async def echo_handler(message: types.Message):
     if message.text.isdigit():
-        squared=int(message.text)**2
+        squared = int(message.text) ** 2
         await message.answer(str(squared))
+    elif message.text.lower() == "game":
+        await message.answer_dice(emoji='🎲')
     else:
-        await message.answer(str(message.text))
-
+        await message.answer(message.text)
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(echo_handler)
--- handlers/quiz.py
+++ handlers/quiz.py
@@ -3,16 +3,13 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import bot
 
 
-
-async def quiz(message: types.Message):
+async def quiz_1(message: types.Message):
     keyboard = InlineKeyboardMarkup()
-    button = InlineKeyboardButton('Далее', callback_data='button_1')
-
+    button = InlineKeyboardButton('Далее', callback_data='quiz_2')
     keyboard.add(button)
 
     question = 'Какое время года?'
-
-    answer = ['Лето', "зима", 'осень', "весна"]
+    answer = ['Лето', 'Зима', 'Осень', 'Весна']
 
     with open('media/test.jpg', 'rb') as photo:
         await bot.send_photo(chat_id=message.from_user.id, photo=photo)
@@ -31,6 +28,10 @@ async def quiz(message: types.Message):
 
 
 async def quiz_2(call: types.CallbackQuery):
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton('Завершить', callback_data='end_quiz')
+    keyboard.add(button)
+
     question = 'Dota2 or CS.GO'
     answer = ['Dota2', 'CS.GO', 'Valve']
 
@@ -40,10 +41,12 @@ async def quiz_2(call: types.CallbackQuery):
         options=answer,
         is_anonymous=True,
         type='quiz',
-        correct_option_id=2
+        correct_option_id=2,
+        reply_markup=keyboard
     )
 
 
+
 def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(quiz, commands=['quiz'])
-    dp.register_callback_query_handler(quiz_2, text='button_1')
+    dp.register_message_handler(quiz_1, commands=['quiz'])
+    dp.register_callback_query_handler(quiz_2, text='quiz_2')
